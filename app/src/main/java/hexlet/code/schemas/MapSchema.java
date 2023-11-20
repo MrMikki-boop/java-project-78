60215ca649f0fe75c07f0c20e3eef508025da755
maks <nickitkacozloff@yandex.ru>
2023-11-20 13:39:08 +0300

    add MapSchema.java

---
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -0,0 +1,42 @@
+package hexlet.code.schemas;
+
+import java.util.Map;
+
+public class MapSchema extends BaseSchema {
+    private boolean requireNonNull = false;
+    private Integer sizeConstraint;
+
+    public MapSchema required() {
+        requireNonNull = true;
+        return this;
+    }
+
+    public MapSchema sizeof(int size) {
+        sizeConstraint = size;
+        return this;
+    }
+
+    @Override
+    public boolean isValid(Object value) {
+        if (requireNonNull && value == null) {
+            return false;
+        }
+
+        if (value == null) {
+            return !required; // Изменено: isValid(null) возвращает true, если не установлено required()
+        }
+
+        if (!(value instanceof Map)) {
+            return false;
+        }
+
+        Map<?, ?> mapValue = (Map<?, ?>) value;
+
+        if (requireNonNull && mapValue.containsValue(null)) {
+            return false;
+        }
+
+        return sizeConstraint == null || mapValue.size() == sizeConstraint;
+    }
+
+}
