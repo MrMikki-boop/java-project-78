5e6ce834a7fcf98a0c72997ecbe9c99eaf9ab965
maks <nickitkacozloff@yandex.ru>
2023-11-20 13:41:21 +0300

    update BaseSchema.java

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -3,8 +3,9 @@ package hexlet.code.schemas;
 public abstract class BaseSchema {
     protected boolean required = false;
 
-    public void required() {
+    public BaseSchema required() {
         required = true;
+        return this;
     }
 
     public abstract boolean isValid(Object value);
