cf824856425a4638bbf591985150f8d1cc0ff941
maks <nickitkacozloff@yandex.ru>
2023-11-20 13:41:05 +0300

    update Validator.java

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,14 +1,18 @@
 package hexlet.code;
 
+import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
-    public StringSchema string() {
+    public final StringSchema string() {
         return new StringSchema();
     }
 
-    public NumberSchema number() {
+    public final NumberSchema number() {
         return new NumberSchema();
+    }
+    public final MapSchema map() {
+        return new MapSchema();
     }
 }
