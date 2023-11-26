7b342cfabbb30eb849bba13cc80c956db9a0f20a
maks <nickitkacozloff@yandex.ru>
2023-11-26 19:00:29 +0300

    update NumberSchema.java

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,37 +1,34 @@
 package hexlet.code.schemas;
 
-public class NumberSchema extends BaseSchema {
-    private boolean positive = false;
-    private Integer min;
-    private Integer max;
+public final class NumberSchema extends BaseSchema {
+
+    private static final String DATA_TYPE = "dataType";
+    private static final String REQUIRED = "required";
+    private static final String POSITIVE = "positive";
+    private static final String RANGE = "range";
+
+    private int firstNumber;
+    private int lastNumber;
+
+    public NumberSchema() {
+        addCheck(DATA_TYPE, value -> (value instanceof Integer) || (value == null));
+    }
+
+    public NumberSchema required() {
+        addCheck(REQUIRED, value -> value instanceof Integer);
+        return this;
+    }
 
     public NumberSchema positive() {
-        positive = true;
+        addCheck(POSITIVE, value -> (value == null) || ((int) value > 0));
         return this;
     }
 
-    public NumberSchema range(int minValue, int maxValue) {
-        this.min = minValue;
-        this.max = maxValue;
+    public NumberSchema range(Integer first, Integer last) {
+        firstNumber = first;
+        lastNumber = last;
+        addCheck(RANGE, value -> (value == null) || ((int) value >= firstNumber && (int) value <= lastNumber));
         return this;
     }
 
-    @Override
-    public boolean isValid(Object value) {
-        if (required && value == null) {
-            return false;
-        }
-
-        if (!(value instanceof Number)) {
-            return false;
-        }
-
-        int intValue = ((Number) value).intValue();
-
-        if (positive && intValue <= 0) {
-            return false;
-        }
-
-        return (min == null || intValue >= min) && (max == null || intValue <= max);
-    }
 }
