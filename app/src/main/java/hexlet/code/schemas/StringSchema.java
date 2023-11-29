5aba760f2546ab68581bd017e32e746036608e96
maks <nickitkacozloff@yandex.ru>
2023-11-29 13:44:18 +0300

    update StringSchema.java

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,41 +1,39 @@
-    package hexlet.code.schemas;
+package hexlet.code.schemas;
 
-    public final class StringSchema extends BaseSchema {
+public final class StringSchema extends BaseSchema {
 
-        // Константы для определения различных проверок в схеме
-        private static final String DATA_TYPE = "dataType";
-        private static final String REQUIRED = "required";
-        private static final String MIN_LENGTH = "minLength";
-        private static final String CONTAINS = "contains";
+    // Константы для определения различных проверок в схеме
+    private static final String DATA_TYPE = "dataType";
+    private static final String REQUIRED = "required";
+    private static final String MIN_LENGTH = "minLength";
+    private static final String CONTAINS = "contains";
 
-        // Переменные для хранения параметров минимальной длины и подстроки
-        private int minLengthNumber;
-        private String content;
+    // Переменные для хранения параметров минимальной длины и подстроки
+    private int minLengthNumber;
+    private String content;
 
-        // Конструктор класса, который добавляет проверку на соответствие типу данных (String или null)
-        public StringSchema() {
-            addCheck(DATA_TYPE, value -> (value instanceof String) || value == null);
-        }
-
-        // Метод, добавляющий проверку на обязательность (должно быть не null и не пустой строкой)
-        public StringSchema required() {
-            addCheck(REQUIRED, value -> (value instanceof String) && (!value.equals("")));
-            return this;
-        }
-
-        // Метод, добавляющий проверку на минимальную длину строки
-        public StringSchema minLength(int number) {
-            minLengthNumber = number;
-            addCheck(MIN_LENGTH, value -> (value == null) || (value.toString().length() >= minLengthNumber));
-            return this;
-        }
-
-        // Метод, добавляющий проверку на наличие определенной подстроки
-        public StringSchema contains(String string) {
-            content = string;
-            addCheck(CONTAINS, value -> (value != null) && (value.toString().contains(content)));
-            return this;
-        }
-
+    // Конструктор класса, который добавляет проверку на соответствие типу данных (String или null)
+    public StringSchema() {
+        addCheck(DATA_TYPE, value -> (value instanceof String) || value == null);
     }
 
+    // Метод, добавляющий проверку на обязательность (должно быть не null и не пустой строкой)
+    public StringSchema required() {
+        addCheck(REQUIRED, value -> (value instanceof String) && (!value.equals("")));
+        return this;
+    }
+
+    // Метод, добавляющий проверку на минимальную длину строки
+    public StringSchema minLength(int number) {
+        minLengthNumber = number;
+        addCheck(MIN_LENGTH, value -> (value == null) || (value.toString().length() >= minLengthNumber));
+        return this;
+    }
+
+    // Метод, добавляющий проверку на наличие определенной подстроки
+    public StringSchema contains(String string) {
+        content = string;
+        addCheck(CONTAINS, value -> (value != null) && (value.toString().contains(content)));
+        return this;
+    }
+}
